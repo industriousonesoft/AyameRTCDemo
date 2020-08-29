aa5403f12a17793c19a14145bcfcffce60aae326
industriousonesoft <industriousonesoft@gmail.com>
2020-08-29 16:07:46 +0800

    Add Remote Screen

--- peer-remotescreen.js
+++ peer-remotescreen.js
@@ -329,6 +329,30 @@ async function doSendPong() {
   ws.send(message);  
 }
 
+function browser() {
+  const ua = window.navigator.userAgent.toLocaleLowerCase();
+  if (ua.indexOf('edge') !== -1) {
+    return 'edge';
+  }
+  else if (ua.indexOf('chrome')  !== -1 && ua.indexOf('edge') === -1) {
+    return 'chrome';
+  }
+  else if (ua.indexOf('safari')  !== -1 && ua.indexOf('chrome') === -1) {
+    return 'safari';
+  }
+  else if (ua.indexOf('opera')   !== -1) {
+    return 'opera';
+  }
+  else if (ua.indexOf('firefox') !== -1) {
+    return 'firefox';
+  }
+  return ;
+}
+
+function isSafari() {
+  return browser() === 'safari';
+}
+
 function myLog(text) {
   logTextInput.value += "\n" + text;
   console.log(text);
